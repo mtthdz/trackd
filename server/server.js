a37dcc3d16b7d1a548583e4d05581f5393c0d774
mtthdz <matt@mtthdz.com>
2022-05-28 12:01:54 -0400

    set up nodemon; mongodb connection

--- server/server.js
+++ server/server.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const cors = require('cors');
-
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 8000;
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri)
+const connection = mongoose.connection;
+
 
 app.use(cors());
 app.use(express.json());
+connection.once('open', () => {
+  console.log('connection to MongoDB database established successfully.');
+})
 
 app.listen(port, () => {
-  console.log(`server is running on port ${port}`);
+  console.log(`server is running on port ${port}.`);
 });
