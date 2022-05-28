aa36f030d643b27c3597b0ea5b5b2483e1e496e5
mtthdz <matt@mtthdz.com>
2022-05-28 12:10:02 -0400

    formatting

--- server/server.js
+++ server/server.js
@@ -6,16 +6,14 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 8000;
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri)
 const connection = mongoose.connection;
-
 
 app.use(cors());
 app.use(express.json());
+mongoose.connect(uri)
 connection.once('open', () => {
   console.log('connection to MongoDB database established successfully.');
 })
-
 app.listen(port, () => {
   console.log(`server is running on port ${port}.`);
 });
