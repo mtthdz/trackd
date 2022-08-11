7633385c9a549d2b52b5c754f0b7da66e4a59569
mtthdz <matt@mtthdz.com>
2022-08-11 17:56:03 -0400

    added router functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import About from './pages/About';
-import bleh from './utils/bleh';
+import { Routes, Route } from 'react-router-dom';
+import Journal from './pages/Journal';
+import Login from './pages/Login';
+import Recipes from './pages/Recipes';
+import TDEE from './pages/TDEE';
+import Nav from './components/Nav';
 
 const App = () => {
-  bleh();
 
   return (
-    <section className="App">
-      <About />
-    </section>
+    <div className="App">
+      <Nav />
+
+      <Routes>
+        <Route path="/" element={ <Login /> } />
+        <Route path="/journal" element={ <Journal /> } />
+        <Route path="/recipes" element={ <Recipes /> } />
+        <Route path="/tdee" element={ <TDEE /> } />
+      </Routes>
+    </div>
   );
 }
 
