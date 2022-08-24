4f508834bc2e74db286253867bbd75b842e06b25
mtthdz <matt@mtthdz.com>
2022-08-24 13:49:51 -0400

    created user schema

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -2,11 +2,55 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: {
+  name: {
     type: String,
     required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  metric: {
+    type: Boolean,
+    default: true,
+  },
+  tdee: {
+    gender: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    weight: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    height: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    activity: {
+      type: String,
+      required: true,
+    },
+    goal: {
+      type: String,
+      required: true,
+    },
+    calculation:  {
+      type: Number,
+      required: true,
+      min: 0,
+    },
   },
 });
 
-const Usser = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
 module.exports = User;
