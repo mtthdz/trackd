8d284ea654791821c67f26f0f7d4257acff9ab41
mtthdz <matt@mtthdz.com>
2022-08-25 11:12:29 -0400

    added base pages

--- client/src/pages/Journal.js
+++ client/src/pages/Journal.js
@@ -1,10 +1,28 @@
 import React from "react";
+import styled from "styled-components";
+import Accordian from "../components/Accordian";
+
+
+const WrapperStyles = styled.div`
+  max-width: 1200px;
+  width: 90%;
+  margin: 0 auto;
+  height: 2000px;
+
+  button {
+    background: none;
+    border: none;
+    outline: none;
+    font-size: 1.6rem;
+  }
+`;
+
 
 const Journal = () => {
   return(
-    <section>
-      <h2>Journal Page</h2>
-    </section>
+    <WrapperStyles>
+      <Accordian />
+    </WrapperStyles>
   );
 }
 
