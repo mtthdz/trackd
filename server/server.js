3d78c23a07bfe84bee607a8016b63e820be2239e
mtthdz <matt@mtthdz.com>
2022-08-25 11:00:23 -0400

    temp router & controller for user

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+// temp
+const User = require("./models/user.model");
+const userRoutes = express.Router();
+
 const app = express();
 const port = process.env.PORT || 8000;
 const uri = process.env.ATLAS_URI;
@@ -17,3 +21,30 @@ connection.once('open', () => {
 app.listen(port, () => {
   console.log(`server is running on port ${port}.`);
 });
+
+
+userRoutes.route('/').get(function(req, res) {
+  User.find({}, function(err, users) {
+    if(err) {
+      console.log(err);
+    } else {
+      res.json(users);
+    }
+  })
+});
+
+
+// create
+userRoutes.route('/add').post(function(req, res) {
+  let newUser = new User(req.body);
+
+  newUser.save()
+    .then(newUser => {
+      res.status(200).json({'newUser': 'new user has been saved.'});
+    })
+    .catch(err => {
+      res.status(400).send('adding new user failed');
+    });
+});
+
+app.use('/users', userRoutes);
