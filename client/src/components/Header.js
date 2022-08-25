f447fc50fda6d2fe976aca5a01e02dcb4a6d38a6
mtthdz <matt@mtthdz.com>
2022-08-25 11:13:46 -0400

    nav component creation

---
+++ client/src/components/Header.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Nav from './Nav';
+import styled from "styled-components";
+
+const HeaderStyles = styled.div`
+  box-shadow: 0px 8px 40px 0px #e2e2e2;
+  background-color: #eee;
+  position: sticky;
+  top: 0px;
+  width: 100%;
+  margin-bottom: 20px;
+`;
+
+const WrapperStyles = styled.div`
+  max-width: 1200px;
+  width: 90%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+
+  button {
+    background: none;
+    border: none;
+    outline: none;
+    font-size: 1.6rem;
+  }
+`;
+
+const Header = () => {
+  return(
+    <HeaderStyles>
+      <WrapperStyles>
+        <Nav />
+        <button>logout</button>
+      </WrapperStyles>
+    </HeaderStyles>
+  );
+}
+
+export default Header;
--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,16 +1,53 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import styled from "styled-components";
+
+
+const StyledNav = styled.nav`
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 2% 0;
+
+  h1 {
+    font-size: 1.6rem;
+    font-weight: normal;
+    margin-right: 80px;
+  }
+
+  nav {
+    ul {
+      display: flex;
+      flex-wrap: nowrap;
+  
+      li {
+        padding: 4px 12px;
+        margin-right: 10px;
+      }
+    }
+  }
+`;
+
+const StyledLink = styled(Link)`
+  color: black;
+`;
+
 
 const Nav = () => {
   return(
-    <nav>
-      <ul>
-        <li><Link to="/">Login</Link></li>
-				<li><Link to="/journal">Journal</Link></li>
-				<li><Link to="/recipes">Recipes</Link></li>
-        <li><Link to="/tdee">Profile</Link></li>
-      </ul>
-    </nav>
+    <StyledNav>
+        <h1>trackd</h1>
+
+        <nav>
+          <ul>
+            <li><StyledLink to="/">Login</StyledLink></li>
+            <li><StyledLink to="/journal">Journal</StyledLink></li>
+            <li><StyledLink to="/recipes">Recipes</StyledLink></li>
+            <li><StyledLink to="/tdee">Profile</StyledLink></li>
+          </ul>
+        </nav>
+    </StyledNav>
   )
 }
 
