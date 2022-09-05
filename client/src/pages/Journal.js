552fe15b4ab6a442043b96aed59ae63a3fb926b8
mtthdz <matt@mtthdz.com>
2022-09-05 10:31:36 -0400

    removed accordian components

--- client/src/pages/Journal.js
+++ client/src/pages/Journal.js
@@ -1,61 +1,10 @@
 import React from "react";
-import styled from "styled-components";
-import AccordianHeader from "../components/AccordianHeader";
-import AccordianRow from "../components/AccordianRow";
-
-const WrapperStyles = styled.div`
-  max-width: 1200px;
-  width: 90%;
-  margin: 0 auto;
-  height: 2000px;
-
-  button {
-    background: none;
-    border: none;
-    outline: none;
-    font-size: 1.6rem;
-  }
-`;
 
 
 // sample data
 const Journal = () => {
-  const today = {
-    tracked: [
-      {
-        name: 'pasta',
-        macros: '40 / 40 / 20',
-        kcal: 800,
-      },
-      {
-        name: 'wilbur burrito',
-        macros: '40 / 40 / 20',
-        kcal: 800,
-      },
-      {
-        name: 'pasta',
-        macros: '40 / 40 / 20',
-        kcal: 800,
-      },
-      {
-        name: 'pasta',
-        macros: '40 / 40 / 20',
-        kcal: 800,
-      }
-    ],
-    totalKcal: 4400,
-  }
-
   return(
     <>
-      <AccordianHeader />
-      {
-        today.tracked.map((value, index) => {
-          return(
-            <AccordianRow name={value.name} macros={value.macros} kcal={value.kcal} />
-          )
-        })
-      }
     </>
   );
 }
