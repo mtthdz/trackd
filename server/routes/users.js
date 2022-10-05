407a53589f84955ec9beea9d35cce27199b90a7a
mtthdz <matt@mtthdz.com>
2022-10-04 22:34:09 -0400

    added firebase admin to server; enabled auth via mongo<>firebase servers

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const User = require("../models/user.model");
+var admin = require("firebase-admin");
+var serviceAccount = require("../trackd-180d9-firebase-adminsdk-fikoi-d82e7c8cf3.json");
+
+// Firebase Admin initialize
+// TODO: remove json from project dir
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 const userRoutes = express.Router();
 
@@ -14,9 +22,34 @@ userRoutes.route('/').get((req, res) => {
   })
 })
 
-// find user via firebase uid
-// if user does not exist, create new user
-// regardless, return user body
+/**
+ * Firebase Auth
+ * 1. (front end) user follows login via firebase auth
+ * 2. (front end) user receives idToken, sends here via fetch
+ * 3. Mongo server will verify idToken with Firebase server
+ *    - on success, decodedtoken returns Firebase user
+ *    - at this point, user has been verified
+ *    - the reason for this is to have Mongo and Firebase servers talk, keeping
+ *      front end completely disengaged (OAuth2.0 protocol)
+ * 
+ * TODO: merge /find to complete getting userdata to front end
+ */
+userRoutes.route('/auth').post(async (req, res) => {
+  let token = req.body.token;
+
+  admin.auth()
+  .verifyIdToken(token)
+  .then((decodedToken) => {
+    const uid = decodedToken.uid;
+    res.status(200).json(uid);
+  })
+  .catch((error) => {
+    res.status(400).send(token);
+  });
+})
+
+
+// TODO: merge into /auth endpoint
 userRoutes.route('/find').post(async (req, res) => {
   let uid = req.body.uid;
   let user = await User.findOne({uid: uid});
