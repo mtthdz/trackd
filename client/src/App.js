95cae8c894df9e39fc8bc7a2835542ef92a99b6c
mtthdz <matt@mtthdz.com>
2022-10-04 23:12:28 -0400

    removed context on app.js due to error throwing

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,6 @@ import Journal from './pages/Journal';
 import Login from './pages/Login';
 import Recipes from './pages/Recipes';
 import Profile from './pages/Profile';
-import { auth } from './utils/Firebase';
 import { onAuthStateChanged } from 'firebase/auth';
 import PrivateRoute from './pages/PrivateRoute';
 
@@ -14,54 +13,40 @@ import PrivateRoute from './pages/PrivateRoute';
 export const UserContext = React.createContext(null);
 
 const App = () => {
-  const [user, setUser] = useState(null);
-
-  /**
-   * changes global state of signed in user
-   * attached to Firebase auth, so no need for
-   * updating state value in child components
-   */
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      setUser(user);
-    })
-  }, [user]);
 
   return (
     <>
-      <UserContext.Provider value={{user, setUser}}>
-        <GlobalStyles />
-          <div className="App">
-              <Routes>
-                <Route path="/" element={ <Login /> } />
+      <GlobalStyles />
+        <div className="App">
+            <Routes>
+              <Route path="/" element={ <Login /> } />
 
-                <Route
-                  path="/journal"
-                  element={
-                    <PrivateRoute>
-                      <Journal />
-                    </PrivateRoute>
-                  } 
-                />
-                <Route
-                  path="/recipes"
-                  element={
-                    <PrivateRoute>
-                      <Recipes />
-                    </PrivateRoute>
-                  }
-                />
-                <Route
-                  path="/profile"
-                  element={
-                    <PrivateRoute>
-                      <Profile />
-                    </PrivateRoute>
-                  }
-                />
-              </Routes>
-          </div>
-      </UserContext.Provider>
+              <Route
+                path="/journal"
+                element={
+                  <PrivateRoute>
+                    <Journal />
+                  </PrivateRoute>
+                } 
+              />
+              <Route
+                path="/recipes"
+                element={
+                  <PrivateRoute>
+                    <Recipes />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/profile"
+                element={
+                  <PrivateRoute>
+                    <Profile />
+                  </PrivateRoute>
+                }
+              />
+            </Routes>
+        </div>
     </>
   );
 }
