897de0922fd601fb71f360c866480f617bf17668
mtthdz <matt@mtthdz.com>
2023-01-23 21:18:22 -0500

    created recipe schemas

--- server/models/recipe.model.js
+++ server/models/recipe.model.js
@@ -1,9 +1,36 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const macroSchema = new Schema({
+    protein: {
+        type: Number,
+        required: true,
+    },
+    carbohydrates: {
+        type: Number,
+        required: true,
+    },
+    fats: {
+        type: Number,
+        required: true,
+    },
+})
+
 const recipeSchema = new Schema({
+    uid: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    macros: {
+        type: macroSchema,
+        required: true,
+    },
+})
 
-});
-
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const Recipe = mongoose.model('Recipe', recipeSchema );
 module.exports = Recipe;
