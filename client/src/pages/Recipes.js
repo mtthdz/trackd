d34145f1c47771427678efb7876ec832bd443cb0
mtthdz <matt@mtthdz.com>
2023-01-23 21:25:10 -0500

    updated recipes list with actual db data

--- client/src/pages/Recipes.js
+++ client/src/pages/Recipes.js
@@ -1,17 +1,19 @@
-import React from "react";
+import React, {useContext, useEffect, useState} from "react";
+import { UserContext } from "../App";
 import styled from "styled-components";
 
 import Header from "../components/sections/Header";
 import AccordianHead from "../components/elements/accordian/AccordianHead";
 import AccordianRow from "../components/elements/accordian/AccordianRow";
+import { getRecipes } from "../api/RecipeEndpoint";
 
 
 // sample data
-const recipeData = [
-  {name: 'pasta', macros: '20 / 100 / 12', kcal: 800},
-  {name: 'burger & fries', macros: ' 40 / 80 / 45', kcal: 1200},
-  {name: 'yoghurt', macros: '5 / 10 / 15', kcal: 250},
-]
+// const recipeData = [
+//   {name: 'pasta', macros: '20 / 100 / 12', kcal: 800},
+//   {name: 'burger & fries', macros: ' 40 / 80 / 45', kcal: 1200},
+//   {name: 'yoghurt', macros: '5 / 10 / 15', kcal: 250},
+// ]
 
 const WrapperStyles = styled.div`
   max-width: 1200px;
@@ -21,13 +23,24 @@ const WrapperStyles = styled.div`
 
 const Recipes = () => {
   // totals kcal from dataset
-  let goalKcal = 3200;
-  let totalKcal = 0;
+  // let goalKcal = 3200;
+  // let totalKcal = 0;
+  
+  let {user} = useContext(UserContext);
+  let [recipes, setRecipes] = useState([]);
 
+  const getUserRecipes = async (uid) => {
+    let fetchRecipes = await getRecipes(uid);
+    setRecipes(fetchRecipes);
+  }
 
-  recipeData.forEach((value) => {
-    totalKcal = totalKcal + value.kcal;
-  });
+  useEffect(() => {
+    getUserRecipes(user.uid);
+  }, [setRecipes]);
+  
+  // recipeData.forEach((value) => {
+  //   totalKcal = totalKcal + value.kcal;
+  // });
 
 
   return(
@@ -40,10 +53,10 @@ const Recipes = () => {
           macros={true}
         />
 
-        {recipeData.length === 0 || recipeData.length === undefined
+        {recipes.length === 0 || recipes.length === undefined
           ? <p>Nothing here yet!</p>
-          : recipeData.map((value, index) => {
-            return <AccordianRow key={index}  name={value.name} macros={value.macros} kcal={value.kcal} />
+          : recipes.map((value, index) => {
+            return <AccordianRow key={index}  name={value.name} macros={0} kcal={0} />
           })
         }
       </WrapperStyles>
