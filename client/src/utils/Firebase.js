1166365ed99855b1cbe5d109831e3ab288a1cc86
mtthdz <matt@mtthdz.com>
2023-01-23 21:22:44 -0500

    removed unused import

--- client/src/utils/Firebase.js
+++ client/src/utils/Firebase.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import {getAuth, signInWithPopup, GoogleAuthProvider, signOut} from 'firebase/auth';
-import { getUser } from '../api/AuthEndpoint';
 
 
 // Firebase configuration
